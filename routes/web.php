86403f72cf4e3007d27aab520b1db11afbd13e47
Ryan Choi <ryan.choi@webadvanced.com>
2017-09-26 16:58:59 -0700

    User registration

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,12 @@
 |
 */
 
-Route::get('/', 'PostController@index');
+Route::get('/', 'PostController@index')->name('home');
 Route::get('/posts/create', 'PostController@create');
 Route::post('/posts', 'PostController@store');
 Route::get('/posts/{post}', 'PostController@show');
 Route::post('/posts/{post}/comments', 'CommentController@store');
+
+Route::get('/register', 'RegistrationController@create');
+Route::post('/register', 'RegistrationController@store');
+Route::get('/login', 'SessionController@create');
