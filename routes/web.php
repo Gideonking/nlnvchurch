588ec435cf20740088f8221af6a5d46a4ec3f06a
scrumptiousdev <ryan.mingyu.choi@gmail.com>
2017-10-04 23:25:11 -0700

    Example of service container

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,13 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+App::bind('App\Billing\Stripe', function () {
+  return new \App\billing\Stripe(config('services.stripe.secret'));
+});
+
+$stripe = resolve('App\Billing\Stripe');
+// $stripe = App::make('App\Billing\Stripe');
+
+dd($stripe);
 
 Route::get('/', 'PostController@index')->name('home');
 Route::get('/home', 'PostController@index')->name('home');
