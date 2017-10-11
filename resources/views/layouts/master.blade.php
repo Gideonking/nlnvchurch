41f695f9a560ffde40179f82a292ef46bbc05746
Ryan Choi <ryan.choi@webadvanced.com>
2017-10-10 18:07:15 -0700

    Starting on navigation design

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -15,24 +15,13 @@
 
     <!-- Custom styles for this template -->
     <link href="/css/app.css" rel="stylesheet">
-    <link href="/css/blog.css" rel="stylesheet">
+
+    <!-- FontWesome -->
+    <script src="https://use.fontawesome.com/798a0438d6.js"></script>
   </head>
 
   <body>
     @include('layouts.nav')
-
-    @if($flash = session('message'))
-      <div id="flash-message" class="alert alert-success" role="alert">
-        {{ $flash }}
-      </div>
-    @endif
-
-    <div class="blog-header">
-      <div class="container">
-        <h1 class="blog-title">The Bootstrap Blog</h1>
-        <p class="lead blog-description">An example blog template built with Bootstrap.</p>
-      </div>
-    </div>
 
     <div class="container">
       <div class="row">
--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,14 +1,42 @@
-<div class="blog-masthead">
-  <div class="container">
-    <nav class="nav">
-      <a class="nav-link active" href="#">Home</a>
-      <a class="nav-link" href="#">New features</a>
-      <a class="nav-link" href="#">Press</a>
-      <a class="nav-link" href="#">New hires</a>
-      <a class="nav-link" href="#">About</a>
-      @if(auth()->check())
-        <a class="nav-link ml-auto" href="#">{{ auth()->user()->name }}</a>
-      @endif
-    </nav>
-  </div>
-</div>
+<nav class="nav">
+  <a class="nav__link" href="#">
+    <i class="nav__icon fa fa-leaf" aria-hidden="true"></i>
+    <p class="nav__text">About</p>
+  </a>
+  <a class="nav__link" href="#">
+    <i class="nav__icon fa fa-users" aria-hidden="true"></i>
+    <p class="nav__text">Staff</p>
+  </a>
+  <a class="nav__link" href="#">
+    <i class="nav__icon fa fa-quote-right" aria-hidden="true"></i>
+    <p class="nav__text">Messages</p>
+  </a>
+  <a class="nav__link" href="#">
+    <i class="nav__icon fa fa-bullhorn" aria-hidden="true"></i>
+    <p class="nav__text">News</p>
+  </a>
+  <a class="nav__link" href="#">
+    <i class="nav__icon fa fa-calendar" aria-hidden="true"></i>
+    <p class="nav__text">Events</p>
+  </a>
+  <a class="nav__link" href="#">
+    <i class="nav__icon fa fa-camera-retro" aria-hidden="true"></i>
+    <p class="nav__text">Gallery</p>
+  </a>
+  <a class="nav__link" href="#">
+    <i class="nav__icon fa fa-heart" aria-hidden="true"></i>
+    <p class="nav__text">Support</p>
+  </a>
+  <a class="nav__link" href="#">
+    <i class="nav__icon fa fa-envelope" aria-hidden="true"></i>
+    <p class="nav__text">Contact</p>
+  </a>
+  <a class="nav__link" href="#">
+    <i class="nav__icon fa fa-map-signs" aria-hidden="true"></i>
+    <p class="nav__text">Directions &amp; Times</p>
+  </a>
+
+  @if(auth()->check())
+    <a class="nav-link ml-auto" href="#">{{ auth()->user()->name }}</a>
+  @endif
+</nav>
