ab30a0eb01205bd739ca0121e7b5aadc65b23f83
scrumptiousdev <ryan.mingyu.choi@gmail.com>
2017-10-11 00:05:37 -0700

    Homepage banner text

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,6 +2,9 @@
 
 @section('content')
   <div class="nlnv-container--full">
-
+    <div class="welcome-text-container">
+      <h1 class="welcome-text welcome-text--top">A church that <span class="welcome-text-accent">plants</span> churches</h1>
+      <h1 class="welcome-text welcome-text--bottom">for the <span class="welcome-text-accent">next</span> generation</h1>
+    </div>
   </div>
 @endsection
