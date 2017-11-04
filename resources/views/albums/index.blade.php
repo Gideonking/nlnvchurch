4e874c179cc344a18a189cc8e244a353abc91a51
scrumptiousdev <ryan.mingyu.choi@gmail.com>
2017-11-04 00:55:22 -0700

    Album layout and card hovering effect

--- resources/views/albums/index.blade.php
+++ resources/views/albums/index.blade.php
@@ -2,59 +2,124 @@
 
 @section('content')
 
+  <div class="album__banner text-center bg--offwhite">
+    <h1 class="nlnv__heading kor-main">사진첩</h1>
+    <hr class="divider divider--green">
+  </div>
+
   <div class="album__container bg--offwhite">
     <div class="album__wrapper">
       <div class="album__card col-md-4">
         <a href="#" class="album__card-inner">
-          <img class="album__card-img" src="https://scontent-sjc2-1.xx.fbcdn.net/v/t1.0-9/22154468_1612635422091034_1300013432688900062_n.jpg?oh=0a0c75afbfe78e62a52f4ceddf49b500&oe=5A64780F" alt="">
+          <div class="album__card-img-wrapper">
+            <i class="album__card-icon fa fa-picture-o" aria-hidden="true"></i>
+            <img class="album__card-img" src="https://scontent-sjc2-1.xx.fbcdn.net/v/t1.0-9/22154419_1612635502091026_5963832553173417571_n.jpg?oh=7b8f6d465b2a4b7d98848be557009aea&oe=5AA16FCE" alt="">
+          </div>
           <div class="album__card-content">
-            <h2 class="kor-main">한글학교<br>(송편 만들기 체험)</h2>
-            <p>23 Photos</p>
+            <h2 class="album__card-title kor-main">한글학교 - 송편 만들기 체험</h2>
+            <div class="row">
+              <div class="col-xs-6">
+                <p class="album__card-info">23 Photos</p>
+              </div>
+              <div class="col-xs-6 text-right">
+                <p class="album__card-date">10/30/2017</p>
+              </div>
+            </div>
           </div>
         </a>
       </div>
       <div class="album__card col-md-4">
         <a href="#" class="album__card-inner">
-          <img class="album__card-img" src="https://scontent-sjc2-1.xx.fbcdn.net/v/t1.0-9/19642317_1518479068173337_2314982937064226376_n.jpg?oh=54bc4130a59fe53f44c94a9606c17ffa&oe=5A76248F" alt="">
+          <div class="album__card-img-wrapper">
+            <i class="album__card-icon fa fa-picture-o" aria-hidden="true"></i>
+            <img class="album__card-img" src="https://scontent-sjc2-1.xx.fbcdn.net/v/t1.0-9/19642317_1518479068173337_2314982937064226376_n.jpg?oh=54bc4130a59fe53f44c94a9606c17ffa&oe=5A76248F" alt="">
+          </div>
           <div class="album__card-content">
-            <h2 class="kor-main">전교인 캠핑</h2>
-            <p>34 Photos</p>
+            <h2 class="album__card-title kor-main">전교인 캠핑</h2>
+            <div class="row">
+              <div class="col-xs-6">
+                <p class="album__card-info">34 Photos</p>
+              </div>
+              <div class="col-xs-6 text-right">
+                <p class="album__card-date">07/02/2017</p>
+              </div>
+            </div>
           </div>
         </a>
       </div>
       <div class="album__card col-md-4">
         <a href="#" class="album__card-inner">
-          <img class="album__card-img" src="https://scontent-sjc2-1.xx.fbcdn.net/v/t1.0-9/15492619_1301575083197071_8143145125929936613_n.jpg?oh=b161f987ebf61411eeabbef005e63a07&oe=5A7043FF" alt="">
+          <div class="album__card-img-wrapper">
+            <i class="album__card-icon fa fa-picture-o" aria-hidden="true"></i>
+            <img class="album__card-img" src="https://scontent-sjc2-1.xx.fbcdn.net/v/t1.0-9/15492619_1301575083197071_8143145125929936613_n.jpg?oh=b161f987ebf61411eeabbef005e63a07&oe=5A7043FF" alt="">
+          </div>
           <div class="album__card-content">
-            <h2 class="kor-main">한글학교<br>(첫 학기)</h2>
-            <p>5 Photos</p>
+            <h2 class="album__card-title kor-main">한글학교 - 첫 학기</h2>
+            <div class="row">
+              <div class="col-xs-6">
+                <p class="album__card-info">5 Photos</p>
+              </div>
+              <div class="col-xs-6 text-right">
+                <p class="album__card-date">12/17/2016</p>
+              </div>
+            </div>
           </div>
         </a>
       </div>
       <div class="album__card col-md-4">
         <a href="#" class="album__card-inner">
-          <img class="album__card-img" src="https://scontent-sjc2-1.xx.fbcdn.net/v/t1.0-9/13494846_1145193395501908_774133519443297860_n.jpg?oh=a85021d7d3f4e1fdf584c84e000582c5&oe=5A689031" alt="">
+          <div class="album__card-img-wrapper">
+            <i class="album__card-icon fa fa-picture-o" aria-hidden="true"></i>
+            <img class="album__card-img" src="http://nlnvchurch.org/wp-content/uploads/2017/09/13509019_1145193165501931_6527062108475169712_n-640x960.jpg" alt="">
+          </div>
           <div class="album__card-content">
-            <h2 class="kor-main">체육대회</h2>
-            <p>16 Photos</p>
+            <h2 class="album__card-title kor-main">체육대회</h2>
+            <div class="row">
+              <div class="col-xs-6">
+                <p class="album__card-info">16 Photos</p>
+              </div>
+              <div class="col-xs-6 text-right">
+                <p class="album__card-date">06/25/2016</p>
+              </div>
+            </div>
           </div>
         </a>
       </div>
       <div class="album__card col-md-4">
         <a href="#" class="album__card-inner">
-          <img class="album__card-img" src="http://nlnvchurch.org/wp-content/uploads/2016/03/image-e1457455402682.jpeg" alt="">
+          <div class="album__card-img-wrapper">
+            <i class="album__card-icon fa fa-picture-o" aria-hidden="true"></i>
+            <img class="album__card-img" src="http://nlnvchurch.org/wp-content/uploads/2016/03/image-e1457455402682.jpeg" alt="">
+          </div>
           <div class="album__card-content">
-            <h2 class="kor-main">성경공부</h2>
-            <p>13 Photos</p>
+            <h2 class="album__card-title kor-main">성경공부</h2>
+            <div class="row">
+              <div class="col-xs-6">
+                <p class="album__card-info">13 Photos</p>
+              </div>
+              <div class="col-xs-6 text-right">
+                <p class="album__card-date">03/08/2016</p>
+              </div>
+            </div>
           </div>
         </a>
       </div>
       <div class="album__card col-md-4">
         <a href="#" class="album__card-inner">
-          <img class="album__card-img" src="http://nlnvchurch.org/wp-content/uploads/2016/02/KakaoTalk_Photo_2016-02-25-19-10-17_95-719x960.jpeg" alt="">
+          <div class="album__card-img-wrapper">
+            <i class="album__card-icon fa fa-picture-o" aria-hidden="true"></i>
+            <img class="album__card-img" src="http://nlnvchurch.org/wp-content/uploads/2016/02/KakaoTalk_Photo_2016-02-25-19-10-31_41.jpeg" alt="">
+          </div>
           <div class="album__card-content">
-            <h2 class="kor-main">교회건물</h2>
-            <p>6 Photos</p>
+            <h2 class="album__card-title kor-main">교회건물</h2>
+            <div class="row">
+              <div class="col-xs-6">
+                <p class="album__card-info">6 Photos</p>
+              </div>
+              <div class="col-xs-6 text-right">
+                <p class="album__card-date">02/25/2016</p>
+              </div>
+            </div>
           </div>
         </a>
       </div>
